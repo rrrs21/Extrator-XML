f201e9ff6624afceccaabe9cc6712dd8966ff34c
rrrs21 <78495496+rrrs21@users.noreply.github.com>
2022-05-19 15:09:15 -0300

    Programa funcional

--- arquivo.java
+++ arquivo.java
@@ -1,71 +1,67 @@
 import java.io.*;
 import javax.swing.*;
+
 public class Extrator {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		java.io.File diretorio = new java.io.File(JOptionPane.showInputDialog("Digite o Diretório completo"));
-		java.io.File arquivo = new java.io.File(diretorio,"resultado.txt");
+		java.io.File arquivo = new java.io.File(diretorio,"resultado.csv");
 		java.io.File arquivoXml = new java.io.File(diretorio, "xml.xml");
-		String linha = " ";
-		Arquivo lArquivo = new Arquivo();
-    
+		int max=0;
 		try {
-			arquivo.createNewFile();
-			while (linha !=null) {
-		        
-				linha = ler(arquivoXml);
-				if (linha.contains("<dadosMercadoriaCodigoNcm>")) {
-					lArquivo.setNcm(linha.substring(26, 34));
-				} else if (linha.contains("<pisCofinsBaseCalculoValor>")){
-	                    	lArquivo.setValor(Float.parseFloat(linha.substring(28, 42))/100);
-	                    	escrever(arquivo, lArquivo);
-	                	}
-			}
-			FileReader fileReader = new FileReader(arquivoXml);
-			fileReader.close();
+			arquivo.createNewFile();	
+			max = ler(arquivoXml,arquivo);
+			JOptionPane.showMessageDialog(null,"Extração concluída com sucesso. Quantidade de adições: "+max);
 		}
 		catch (IOException e) {
 			System.out.println("erro"+e.toString());	
 		}
-    
-    	
-		
 	}
 
-	public static void escrever (File arq, Arquivo lArquivo){
+	public static void escrever (File arq, Arquivo lArquivos){
 		try {			
 			FileWriter fileWriter= new FileWriter(arq,true);
 			PrintWriter printWriter = new PrintWriter(fileWriter);
-			printWriter.print(lArquivo.getNcm());
-			printWriter.print(" - ");
-			printWriter.println(lArquivo.getValor());
+			printWriter.print(lArquivos.getNcm());
+			printWriter.print(";");
+			printWriter.printf("%4.2f\n", lArquivos.getValor());
 			printWriter.flush();
 			printWriter.close();
-			} catch (IOException e) {
-				System.out.println("erro"+e.toString());
-				}	
+		} catch (IOException e) {
+			System.out.println("erro"+e.toString());
+		}	
 	}
 
-	public static String ler (File arq){
+	public static int ler (File arq, File rFile){
 		String linha= "";
+		int i=0;
 		try {			
 			FileReader fileReader = new FileReader(arq);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-		    linha= bufferedReader.readLine();
-		    bufferedReader.close();
+			linha= bufferedReader.readLine();
+			Arquivo auxiliArquivo = new Arquivo("0",0);
+			while (linha !=null) {
+				if (linha.contains("<dadosMercadoriaCodigoNcm>")) {
+					auxiliArquivo.setNcm(linha.substring(38, 46));
+				} else if (linha.contains("<pisCofinsBaseCalculoValor>")){
+							auxiliArquivo.setValor(Float.parseFloat(linha.substring(39, 54))/100);
+						escrever(rFile, auxiliArquivo);
+							i++;
+						}
+				linha= bufferedReader.readLine();
+			}
+			bufferedReader.close();
 		} catch (IOException e) {
 			System.out.println("erro"+e.toString());
 		}
-		return linha;
+		return i;
 	}
 }
 
 class Arquivo{
 
-	String ncm = null;
-	float valor = 0;
+	public String ncm = null;
+	public float valor = 0;
 
 	public static void main(String[] args) {
 			
@@ -82,8 +78,14 @@ class Arquivo{
 		return valor;
 	}
 
+	public Arquivo(String ncm, float valor) {
+		this.ncm = ncm;
+		this.valor = valor;
+	}
 	public void setValor(float valor) {
 		this.valor = valor;
+	}
+	public Arquivo() {
 	}
 
 }
